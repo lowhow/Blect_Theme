1a66a4166e5e041c01fe7f57b63d79cc516f2aa6
Low How <lowhow@gmail.com>
2017-10-02 23:42:00 +0800

    change pagination

--- search.php
+++ search.php
@@ -1,13 +1,9 @@
 <?php
 /**
  * The template for general search result page
- *
- * @package WordPress
- * @subpackage Vun_Hougkh
- * @since The Starry Night 1.0 with Vun Hougkh 1.0
  */
-
-get_header(); ?>
+get_header();
+global $helper; ?>
 <?php get_template_part( 'partials/page-header/search' ); ?>
 <div class="page-body">
 	<div class="page-body-container container">
@@ -22,7 +18,7 @@ get_header(); ?>
 						while ( have_posts() ) : the_post(); 
 							get_template_part( 'partials/content/searchresults' );
 						endwhile; 
-						$helper->t_pagination();
+						echo $helper->bootstrap_pagination();
 					?>
 					<script>
 
