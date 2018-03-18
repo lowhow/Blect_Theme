293fcd1d45db7b0f6c84079b50a734e3fbd81a08
Low How <lowhow@gmail.com>
2018-03-18 18:21:46 +0800

    add prevd() and current_url()

--- functions.php
+++ functions.php
@@ -11,3 +11,13 @@
  * Bootstrapping framework to WordPress theme and initialise.
  */
 require get_template_directory() . '/blect-config.php';
+
+function prevd($object){
+	echo '<pre>';
+	var_dump($object);
+	echo '</pre>';
+}
+
+function current_url(){
+	return $current_url =  trailingslashit('http' . (isset($_SERVER['HTTPS']) ? 's' : '') . '://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
+}
