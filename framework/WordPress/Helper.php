4d399e315b25d0d1a0b49a31a4c9fbe40b7df1ad
Low How <lowhow@gmail.com>
2018-10-02 22:22:08 +0800

    change favicon to favicon generator format

--- framework/WordPress/Helper.php
+++ framework/WordPress/Helper.php
@@ -37,13 +37,29 @@ class Helper
 
 	/**
 	 * Add favicon links
+	 *
+	 * @link https://www.favicon-generator.org/
 	 */
 	public function add_favicon() {
-		echo '<link rel="apple-touch-icon-precomposed" sizes="144x144" href="'. FW_THEME_ASSETS_FAVICON_URI .'apple-touch-icon-144-precomposed.png" />' .
-		'<link rel="apple-touch-icon-precomposed" sizes="114x114" href="'. FW_THEME_ASSETS_FAVICON_URI .'apple-touch-icon-114-precomposed.png" /> ' .
-	  '<link rel="apple-touch-icon-precomposed" sizes="72x72" href="'. FW_THEME_ASSETS_FAVICON_URI .'apple-touch-icon-72-precomposed.png" />' .
-	  '<link rel="apple-touch-icon-precomposed" href="'. FW_THEME_ASSETS_FAVICON_URI .'apple-touch-icon-57-precomposed.png" />' .
-	  '<link rel="shortcut icon" href="'. FW_THEME_ASSETS_FAVICON_URI .'favicon.ico" />';
+		echo '
+			<link rel="apple-touch-icon" sizes="57x57" href="'. FW_THEME_ASSETS_FAVICON_URI .'apple-icon-57x57.png">
+			<link rel="apple-touch-icon" sizes="60x60" href="'. FW_THEME_ASSETS_FAVICON_URI .'apple-icon-60x60.png">
+			<link rel="apple-touch-icon" sizes="72x72" href="'. FW_THEME_ASSETS_FAVICON_URI .'apple-icon-72x72.png">
+			<link rel="apple-touch-icon" sizes="76x76" href="'. FW_THEME_ASSETS_FAVICON_URI .'apple-icon-76x76.png">
+			<link rel="apple-touch-icon" sizes="114x114" href="'. FW_THEME_ASSETS_FAVICON_URI .'apple-icon-114x114.png">
+			<link rel="apple-touch-icon" sizes="120x120" href="'. FW_THEME_ASSETS_FAVICON_URI .'apple-icon-120x120.png">
+			<link rel="apple-touch-icon" sizes="144x144" href="'. FW_THEME_ASSETS_FAVICON_URI .'apple-icon-144x144.png">
+			<link rel="apple-touch-icon" sizes="152x152" href="'. FW_THEME_ASSETS_FAVICON_URI .'apple-icon-152x152.png">
+			<link rel="apple-touch-icon" sizes="180x180" href="'. FW_THEME_ASSETS_FAVICON_URI .'apple-icon-180x180.png">
+			<link rel="icon" type="image/png" sizes="192x192"  href="'. FW_THEME_ASSETS_FAVICON_URI .'android-icon-192x192.png">
+			<link rel="icon" type="image/png" sizes="32x32" href="'. FW_THEME_ASSETS_FAVICON_URI .'favicon-32x32.png">
+			<link rel="icon" type="image/png" sizes="96x96" href="'. FW_THEME_ASSETS_FAVICON_URI .'favicon-96x96.png">
+			<link rel="icon" type="image/png" sizes="16x16" href="'. FW_THEME_ASSETS_FAVICON_URI .'favicon-16x16.png">
+			<link rel="manifest" href="'. FW_THEME_ASSETS_FAVICON_URI .'manifest.json">
+			<meta name="msapplication-TileColor" content="#ffffff">
+			<meta name="msapplication-TileImage" content="'. FW_THEME_ASSETS_FAVICON_URI .'ms-icon-144x144.png">
+			<meta name="theme-color" content="#ffffff">
+		';
 
 	  return $this;
 	}
